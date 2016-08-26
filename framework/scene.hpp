537ac1da8e889de2c56f9a6add742999218eba88
JaniTomati <jana.puschmann@uni-weimar.de>
2016-08-26 22:03:59 +0200

    Only cosmetic changes (conversion from indenting via tabs to spaces)

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -18,12 +18,11 @@ struct Scene {
 	 // member
   unsigned int width; 
   unsigned int height; 
-	std::vector<std::shared_ptr<Shape>> shapes_;
-	std::map<std::string, Material> materials_; 
-	std::shared_ptr<Camera> cam_;
-	// std::vector<shared_ptr<LightSource>> light_; 
-    // Light Object still has to be added to the framework 
-
+  std::vector<std::shared_ptr<Shape>> shapes_;
+  std::map<std::string, Material> materials_; 
+  std::shared_ptr<Camera> cam_;
+  std::vector<shared_ptr<LightSource>> light_; 
+  
 };
 
 Scene loadSDF(std::string const& file_in);
