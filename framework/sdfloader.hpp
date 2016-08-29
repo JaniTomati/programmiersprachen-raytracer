9182d4e52907cdc078b5700cd77f06d2480653a4
JaniTomati <jana.puschmann@uni-weimar.de>
2016-08-29 13:59:22 +0200

    Only some minor changes in SDFloader

--- framework/sdfloader.hpp
+++ framework/sdfloader.hpp
@@ -3,19 +3,6 @@
 #ifndef BUW_SDFLOADER_HPP
 #define BUW_SDFLOADER_HPP
 #include "scene.hpp"
-#include "material.hpp"
-#include "shape.hpp"
-#include "sphere.hpp"
-#include "box.hpp"
-#include "camera.hpp"
-#include "lightsource.hpp"
-#include "color.hpp"
-#include <iostream>      
-#include <fstream>
-#include <sstream> 
-#include <string>
-#include <map>
-#include <vector>
 
 struct SDFloader {
 
