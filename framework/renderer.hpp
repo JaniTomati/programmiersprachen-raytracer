f0e07af30bad6b002d1f4a07ab6f641d3c6c5502
JaniTomati <jana.puschmann@uni-weimar.de>
2016-08-29 13:58:35 +0200

    raytrace function needs to be written. :mega:

--- framework/renderer.hpp
+++ framework/renderer.hpp
@@ -9,27 +9,33 @@
 
 #ifndef BUW_RENDERER_HPP
 #define BUW_RENDERER_HPP
-
+#include "scene.hpp"
 #include "color.hpp"
+#include "ray.hpp"
+#include "shape.hpp"
+#include "sphere.hpp"
 #include "pixel.hpp"
 #include "ppmwriter.hpp"
 #include <string>
 #include <glm/glm.hpp>
 
-class Renderer
-{
+class Renderer {
+
 public:
   Renderer(unsigned w, unsigned h, std::string const& file);
 
   void render();
   void write(Pixel const& p);
+  Color raytrace(Ray const& ray, unsigned int depth);
 
-  inline std::vector<Color> const& colorbuffer() const
-  {
+  // Color shade(OptiHit const&, Ray const& ray, unsigned int depth);
+
+  inline std::vector<Color> const& colorbuffer() const {
     return colorbuffer_;
   }
 
 private:
+  Scene scene_;
   unsigned width_;
   unsigned height_;
   std::vector<Color> colorbuffer_;
